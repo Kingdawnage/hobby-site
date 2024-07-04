902f738e2c80c3f97eda85786fdc6002af254c44
Kingdawnage <katchymemo@yahoo.com>
2024-07-04 16:00:15 +0100

    Header implementation with Title and Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+    return ( 
+        <div className="Navbar">
+            <div className="Nav-1">
+                <span className="hash">#</span><span className="title">home</span>
+            </div>
+            <div className="Nav-2">
+                <span className="hash">#</span><span className="title">projects</span>
+            </div>
+            <div className="Nav-3">
+                <span className="hash">#</span><span className="title">about-me</span>
+            </div>
+            <div className="Nav-3">
+                <span className="hash">#</span><span className="title">contacts</span>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
