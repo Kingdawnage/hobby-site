52301a2ee94919eb1af3889845b9133deb4668be
Kingdawnage <katchymemo@yahoo.com>
2024-07-07 17:26:56 +0100

    Made Sectionheader derivative without dropdown link

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,46 @@
-import './App.css';
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Aboutme from './components/Aboutme'
-import Marquee from './components/Marquee';
-import Sectionheader from './components/Sectionheader';
-import Exhibition from './components/Exhibition';
-import Activity from './components/Activity';
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Aboutme from "./components/Aboutme";
+import Marquee from "./components/Marquee";
+import {
+  SectionHeaderDropdownless,
+  Sectionheader,
+} from "./components/Sectionheader";
+import Exhibition from "./components/Exhibition";
+import Activity from "./components/Activity";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <div className="app-content">
         <div className="Container">
-          <Header
-              title='King'/>
-          <Navbar
-              tag1='home'
-              tag2='projects'
-              tag3='about-me'
-              tag4='contacts'/>
+          <Header title="King" />
+          <Navbar tag1="home" tag2="projects" tag3="about-me" tag4="contacts" />
         </div>
         <div className="Container-1">
-          <Aboutme/>
+          <Aboutme />
         </div>
         <div className="Container-2">
-          <Marquee/>
+          <Marquee />
         </div>
         <div className="Container-3">
-          <Sectionheader
-            title='Projects'/>
+          <Sectionheader title="Projects" />
         </div>
         <div className="Container-4">
-          <Exhibition/>
+          <Exhibition />
         </div>
         <div className="Container-5">
-          <Sectionheader
-            title='My Activity'/>
+          {/* <Sectionheader title="My Activity" /> */}
+          <SectionHeaderDropdownless title="My Activity" />
         </div>
         <div className="Container-6">
-          <Activity/>
+          <Activity />
         </div>
-        <div className="Container-7">
-
-        </div>
+        <div className="Container-7"></div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
